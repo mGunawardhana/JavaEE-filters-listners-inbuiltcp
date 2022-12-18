64158fbe1872ccc2c069df392b656d80bf550e74
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-12-18 14:53:42 +0530

    filters

---
+++ src/servlet/A.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * CopyWriteOwner - mr.Gunawardhana
+ * Contact - 071 - 733 1792
+ *
+ * © 2022 mGunawardhana,INC. ALL RIGHTS RESERVED.
+ */
+
+@WebServlet(urlPatterns = "/a")
+public class A extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet A doGet Method Invoked");
+    }
+}
---
+++ src/servlet/B.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import sun.net.httpserver.HttpServerImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * CopyWriteOwner - mr.Gunawardhana
+ * Contact - 071 - 733 1792
+ *
+ * © 2022 mGunawardhana,INC. ALL RIGHTS RESERVED.
+ */
+
+@WebServlet(urlPatterns = "/b")
+public class B extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Servlet B Get Method Invoked");
+    }
+}
