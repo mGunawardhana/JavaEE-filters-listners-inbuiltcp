92e4d549690229b8d8d64c059b9db0def6512319
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-12-18 15:16:10 +0530

    introducing filter

---
+++ src/filters/filterOne.java
@@ -0,0 +1,10 @@
+package filters;
+
+/**
+ * CopyWriteOwner - mr.Gunawardhana
+ * Contact - 071 - 733 1792
+ * <p>
+ * © 2022 mGunawardhana,INC. ALL RIGHTS RESERVED.
+ */
+public class filterOne {
+}
